a4e82f2a35cbf4357307f28ee59e4451bce435c0
Abhijeet Tiwari <101239858+itsabhi739@users.noreply.github.com>
2025-02-07 17:25:53 +0530

    Changing the country code in App.js

--- src/App.js
+++ src/App.js
@@ -41,13 +41,13 @@ export default class App extends Component {
       </div>
      
       <Routes>
-           <Route exact path='/' element={<News setProgress = {this.setProgress} key = {'general'} pageSize = {9} country ="in" category = {'general'}/>} />
-           <Route exact path='/business' element={<News setProgress = {this.setProgress} key = {'business'} pageSize = {9} country ="in" category = {'business'}/>} />
-           <Route exact path='/entertainment' element={<News setProgress = {this.setProgress} key = {'entertainment'} pageSize = {9} country ="in" category = {'entertainment'}/>}/>
-           <Route exact path='/health' element={<News setProgress = {this.setProgress} key = {'health'} pageSize = {9} country ="in" category = {'health'}/>} />
-           <Route exact path='/science' element={<News setProgress = {this.setProgress} key = {'science'} pageSize = {9} country ="in" category = {'science'}/>} />
-           <Route exact path='/sports' element={<News setProgress = {this.setProgress} key = {'sports'} pageSize = {9} country ="in" category = {'sports'}/>} />
-           <Route exact path='/technology' element={<News setProgress = {this.setProgress} key = {'technology'} pageSize = {9} country ="in" category = {'technology'}/>} />
+           <Route exact path='/' element={<News setProgress = {this.setProgress} key = {'general'} pageSize = {9} country ="us" category = {'general'}/>} />
+           <Route exact path='/business' element={<News setProgress = {this.setProgress} key = {'business'} pageSize = {9} country ="us" category = {'business'}/>} />
+           <Route exact path='/entertainment' element={<News setProgress = {this.setProgress} key = {'entertainment'} pageSize = {9} country ="us" category = {'entertainment'}/>}/>
+           <Route exact path='/health' element={<News setProgress = {this.setProgress} key = {'health'} pageSize = {9} country ="us" category = {'health'}/>} />
+           <Route exact path='/science' element={<News setProgress = {this.setProgress} key = {'science'} pageSize = {9} country ="us" category = {'science'}/>} />
+           <Route exact path='/sports' element={<News setProgress = {this.setProgress} key = {'sports'} pageSize = {9} country ="us" category = {'sports'}/>} />
+           <Route exact path='/technology' element={<News setProgress = {this.setProgress} key = {'technology'} pageSize = {9} country ="us" category = {'technology'}/>} />
            <Route exact path='/aboutus' element={<AboutUs/>} />
            <Route exact path='/contactus' element={<Contact/>} />
            <Route exact path='/search' element={<Search /> } /> 
